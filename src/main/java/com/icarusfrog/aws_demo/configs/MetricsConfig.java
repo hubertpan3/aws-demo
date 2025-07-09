7449ea5602ed1e4faca1453fd63d5ea344e3ed2a
Hubert Pan <hubert.pan3@gmail.com>
2025-07-09 06:21:09 -0400

    start aws cloudwatch metrics work

--- src/main/java/com/icarusfrog/aws_demo/configs/MetricsConfig.java
+++ src/main/java/com/icarusfrog/aws_demo/configs/MetricsConfig.java
@@ -1,12 +1,16 @@
 package com.icarusfrog.aws_demo.configs;
 
+import io.micrometer.cloudwatch2.CloudWatchConfig;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
 import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 
 @Slf4j
 @Configuration
@@ -16,6 +20,12 @@ public class MetricsConfig {
     public MeterRegistry localMeterRegistry() {
         log.info("Configuring local simple meter registry.");
         return new SimpleMeterRegistry();
+    }
+
+    @Profile("aws")
+    @Bean
+    public MeterRegistry cloudWatchMeterRegistry(CloudWatchConfig cwc) {
+        return new CloudWatchMeterRegistry(cwc, Clock.SYSTEM, CloudWatchAsyncClient.create());
     }
 
     @Bean
