4af055e09f7a5e725db7fbd87a0f60f2a397227e
Hubert Pan <hubert.pan3@gmail.com>
2025-07-14 06:32:44 -0400

    add log to ping call

--- src/main/java/com/icarusfrog/aws_demo/controllers/ApiController.java
+++ src/main/java/com/icarusfrog/aws_demo/controllers/ApiController.java
@@ -1,10 +1,12 @@
 package com.icarusfrog.aws_demo.controllers;
 
 import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api")
 @RestController
 public class ApiController {
@@ -12,6 +14,7 @@ public class ApiController {
     @Timed(value = "api.ping")
     @GetMapping("/ping")
     public String ping() {
+        log.info("ping called");
         return "OK";
     }
 }
