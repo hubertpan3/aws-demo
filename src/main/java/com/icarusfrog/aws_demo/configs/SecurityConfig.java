4e43f1db4aeee5c1c655697dc8cfe23a758d1f89
Hubert Pan <hubert.pan3@gmail.com>
2025-07-14 07:00:16 -0400

    fix issue where we couldn't see the default login page.

--- src/main/java/com/icarusfrog/aws_demo/configs/SecurityConfig.java
+++ src/main/java/com/icarusfrog/aws_demo/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.icarusfrog.aws_demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,9 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("/api/**", "/actuator/**").permitAll()
+                auth -> auth.requestMatchers("/login", "/error" ,"/api/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated()
         ).csrf(
                 csrf -> csrf.ignoringRequestMatchers("/api/**", "/actuator/**")
+        ).formLogin(Customizer.withDefaults()
+        ).logout(
+                Customizer.withDefaults()
         );
         return http.build();
     }
